fbf983a85a153294a1622e535807bd14ec6fe742
Eduard0w <dudusouza5595@gmail.com>
2025-10-18 07:48:16 -0300

    form completo

--- js/form.js
+++ js/form.js
@@ -1,27 +1,42 @@
-
 //class contato
+let informacoes = [];
 
-class contato {
-    
+class Contato {
+  constructor(nome, sobrenome, email, cpf, telefone, contato) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.email = email;
+    this.cpf = cpf;
+    this.telefone = telefone;
+    this.contato = contato;
+  }
 }
 
 function Post(form) {
+  //previne a auto atualização, quando clicado em enviar.
+  event.preventDefault();
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
+  let data = new Contato(
+    form.elements.namedItem("nome").value,
+    form.elements.namedItem("sobrenome").value,
+    form.elements.namedItem("email").value,
+    form.elements.namedItem("cpf").value,
+    form.elements.namedItem("telefone").value,
+    form.elements.namedItem("contato").value
+  );
+  informacoes.push(data);
+  Enviar();
 }
 
 function Enviar() {
+  console.log("enviou");
+  var nome = document.getElementById("nomeid");
 
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
-
+  if (nome.value != "") {
+    alert(
+      "Obrigado sr(a) " +
+        nome.value +
+        " os seus dados foram encaminhados com sucesso"
+    );
+  }
 }
